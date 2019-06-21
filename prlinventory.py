7f7d1278c1208fa585767729b510b0d65a572819
Pete Ezzo <peter.ezzo1@verizon.com>
2019-06-21 11:23:24 -0400

    refactor parallels inventory plugin to check ssh auth method and use proper output format

--- prlinventory.py
+++ prlinventory.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
+"""
+Parallels Dynamic Inventory Plugin for Ansible
+
+Author: Pete Ezzo <peter.ezzo1@verizon.com>
+"""
 
 import argparse
 import json
-import paramiko
-import socket
 import subprocess
+import paramiko
 
 
-def get_ssh_auth_methods(ip, port):
-    s = socket.socket()
-    s.connect((ip, port))
-    t = paramiko.Transport(s)
-    t.connect()
-
+def _get_ssh_auth_methods(ip, port):
+    t = paramiko.Transport(f'{ip}:{port}')
     try:
+        t.connect()
         t.auth_none('')
     except paramiko.BadAuthenticationType as err:
         return err.allowed_types
+    except paramiko.SSHException:
+        pass
+
+    return ''
 
 
-def get_pvm_list():
+def _get_pvm_list():
     r = subprocess.run(['prlctl', 'list', '-jif', '--all'], capture_output=True)
     r.check_returncode()
     return json.loads(r.stdout)
 
 
-def get_portforward_list():
+def _get_portforward_list():
     r = subprocess.run(['prlsrvctl', 'net', 'info', 'Shared', '-j'], capture_output=True)
     r.check_returncode()
     try:
@@ -35,48 +40,74 @@ def get_portforward_list():
     return forwards
 
 
-def list_hosts():
-    hosts = []
-    for vm in get_pvm_list():
-        if 'kali' in vm['Name'] and 'running' in vm['State']:
-            hosts.append(vm['Name'])
-
-    vagrant_vars = {'ansible_user': 'vagrant', 'ansible_become_pass': 'vagrant'}
-    inventory = {'kali': {'hosts': hosts, 'vars': vagrant_vars}}
-
-    if len(hosts) > 0:
-        print(json.dumps(inventory))
-    else:
-        print(json.dumps({}))
-
-
-def host_vars(searchstring):
-    for vm in get_pvm_list():
-        if vm['Name'] == searchstring and 'running' in vm['State']:
+def list_hosts(searchstring='kali'):
+    """
+    Return a python dict of all hosts with their variables
+    """
+    hosts = {}
+    for vm in _get_pvm_list():
+        if searchstring in vm['Name'] and 'running' in vm['State']:
+            host = vm['Name']
             uid = vm['ID']
 
-            # first check for a portforward
-            for nat in get_portforward_list().values():
-                if uid in nat['destination IP/VM id'] and nat['destination port'] == 22:
-                    vars = {'ansible_host': '127.0.0.1', 'ansible_ssh_port': nat['source port']}
+            hosts[host] = host_vars(host, uid)
 
-                    # second check if auth is password (base box) or key (repeat) because ansible won't let both work
-                    if 'password' in get_ssh_auth_methods('127.0.0.1', nat['source port']):
-                        vars['ansible_ssh_pass'] = 'vagrant'
+    return hosts
 
-                    return vars
 
-            # else go direct for the address
-            mac = vm['Hardware']['net0']['mac']
-            # FIXME: make it work based on IP rather than implicit DNS
-            # grep $mac /Library/Preferences/Parallels/parallels_dhcp_leases
+def print_inventory():
+    """
+    Return a json blob in ansible-inventory format of all running parallels hosts with kali in their name
+    """
+    inventory = {
+        '_meta': {'hostvars': {}},
+        'all': {'children': ['kali', 'ungrouped'], 'hosts': []},
+        'kali': {'children': [], 'hosts': []},
+        'ungrouped': {'children': [], 'hosts': []}
+        }
 
-    # catchall empty set
-    return {}
+    hosts = list_hosts()
+    for host, variables in hosts.items():
+        inventory['kali']['hosts'].append(host)
+        inventory['_meta']['hostvars'][host] = variables
+
+    print(json.dumps(inventory))
+
+
+def host_vars(host, uid):
+    """
+    Return a python dict of all variables for a host, verbosely
+    """
+    variables = {
+        'ansible_user': 'vagrant',
+        'ansible_become_pass': 'vagrant',
+        'ansible_host': host,
+        'ansible_ssh_port': 22
+        }
+
+    # first check is there a local host port forward to vm port 22
+    for nat in _get_portforward_list().values():
+        if uid in nat['destination IP/VM id'] and nat['destination port'] == 22:
+            variables['ansible_host'] = '127.0.0.1'
+            variables['ansible_ssh_port'] = nat['source port']
+
+    # second check if auth is password (base box) or key (repeat) because ansible won't let both work
+    if 'password' in _get_ssh_auth_methods(variables['ansible_host'], variables['ansible_ssh_port']):
+        variables['ansible_ssh_pass'] = 'vagrant'
+
+    return variables
 
 
 def print_host_with_vars(searchstring):
-    print(json.dumps(host_vars(searchstring)))
+    """
+    Print a json dict of the variables for the host matching searchstring
+    """
+    try:
+        hostdata = list_hosts(searchstring)[searchstring]
+    except KeyError:
+        hostdata = {}
+
+    print(json.dumps(hostdata))
 
 
 if __name__ == "__main__":
@@ -91,4 +122,4 @@ if __name__ == "__main__":
     if args.host:
         print_host_with_vars(args.host)
     else:
-        list_hosts()
+        print_inventory()
